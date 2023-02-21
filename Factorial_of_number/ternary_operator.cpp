96620ba29c404cfae435e59afd7549fcf631786d
Shivam kumar singh <sm1788@srmist.edu.in>
2023-02-22 00:57:55 +0530

    Create ternary_operator.cpp

---
+++ Factorial_of_number/ternary_operator.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int factorial (int n){
+    return(n==0||n==1)?1:n*factorial(n-1);
+   
+
+    }
+
+int main()
+{
+    int a;
+    cin>>a;
+    cout<<factorial(a);
+    
+    
+    return 0;
+}
