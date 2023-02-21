5ecfdf574183a7d724a7bc12aa99687f1aad6cda
Shivam kumar singh <sm1788@srmist.edu.in>
2023-02-22 00:51:43 +0530

    Create itreative_method.cpp

---
+++ Factorial_of_number/itreative_method.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int factorial (int n){
+    if (n == 0){
+        cout<<0<<endl;
+    }
+    if(n==1){
+        cout<<1<<endl;
+    }
+    int res=1;
+    for(int i=2;i<=n;i++){
+        res=res*i;
+
+    }
+    return res;
+}
+
+
+int main()
+{
+    int a;
+    cin>>a;
+    cout<<factorial(a);
+    
+    
+    return 0;
+}
