f89e651ccab382aafd55a8921184c40988a09400
Shivam kumar singh <sm1788@srmist.edu.in>
2023-02-22 00:54:44 +0530

    Create recurise_method.cpp

---
+++ Factorial_of_number/recurise_method.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int factorial (int n){
+    if (n == 0 || n==1){
+        return 1;
+    }
+    else{
+        return n*factorial(n-1);
+
+    }
+    
+
+   
+}
+
+
+int main()
+{
+    int a;
+    cin>>a;
+    cout<<factorial(a);
+    
+    
+    return 0;
+}
