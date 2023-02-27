328994698df90b297cf03373cc301e3d1005dfe6
Shivam kumar singh <sm1788@srmist.edu.in>
2023-02-27 16:37:33 +0530

    Update Factorial_of_a_number.cpp

--- Factorial_of_a_number.cpp
+++ Factorial_of_a_number.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+using namespace std;
+int factorial (int n){
+if(n==1||n==0){
+    return 1;
+}else{
+    return (n)*factorial(n-1);
+}
+}
 
+int main()
+{
+    int a=3;
+    cout<<factorial(a)<<endl;
+    
+    return 0;
+}
