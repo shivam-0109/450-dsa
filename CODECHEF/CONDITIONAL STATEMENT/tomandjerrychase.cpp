6baa1629fd503ca1edbe2b534afcc202c95b08b7
Shivam kumar singh <sm1788@srmist.edu.in>
2023-03-01 21:55:24 +0530

    Create tomandjerrychase.cpp
    https://www.codechef.com/problems/JERRYCHASE

---
+++ CODECHEF/CONDITIONAL STATEMENT/tomandjerrychase.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int a ,b;
+	    cin>>a>>b;
+	    if(a>b){
+	        cout<<"NO"<<endl;
+	    }else if(a==b){
+	        cout<<"NO"<<endl;
+	        }else{
+	        cout<<"YES"<<endl;
+	    }
+	}
+	return 0;
+}
